5f0d30ac941eef5647681d658051c5a11ad0c0e9
dilayuluerguven <uluerguven245@gmail.com>
2025-12-02 14:39:00 +0300

    Updated Ganache network config, deploy script, and Docker Compose setup. Added latest certificate file.

--- dapp/hardhat.config.js
+++ dapp/hardhat.config.js
@@ -1,11 +1,14 @@
 require("@nomicfoundation/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-  },
+    ganache: {
+      url: "http://172.23.0.2:8545",
+      accounts: [
+        // Ganache account #0 private key — ETH dolu olan
+        "0x6910fa30e0e81b14b59ce1f9f480abd8a654314fcbd9d55f735153ecfce59924"
+      ]
+    }
+  }
 };
--- dapp/scripts/deploy.js
+++ dapp/scripts/deploy.js
@@ -3,8 +3,15 @@ const hre = require("hardhat");
 async function main() {
   console.log("Deploying CertificateRegistry...");
 
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Using deployer:", await deployer.getAddress());
+  console.log("Balance:", (await hre.ethers.provider.getBalance(deployer.getAddress())).toString());
+
   const Registry = await hre.ethers.getContractFactory("CertificateRegistry");
   const reg = await Registry.deploy();
+
+  await reg.waitForDeployment();
 
   console.log("Contract deployed at:", await reg.getAddress());
 }
